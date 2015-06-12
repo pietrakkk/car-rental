7a1f07c1284b9dcaf3974c7fa70941f6507a49ec
Maciej Borkowski <borek199@gmail.com>
2015-06-12 18:18:23 +0200

    Added front view for searcher

---
+++ app/scripts/searcher/searcherCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('CarRentalApp').controller('SearcherCtrl', SearcherCtrl);
+
+
+function SearcherCtrl($scope, $rootScope) {
+    var BASE_URL = 'http://localhost:9000/#';
+
+}
