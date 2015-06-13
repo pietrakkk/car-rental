d102e8603962e5063320e1b237cf2552bd497289
Maciej Borkowski <borek199@gmail.com>
2015-06-13 22:57:34 +0200

    Suggesting cars for logged user

--- rest-api/service/rentalSuggestionService.js
+++ rest-api/service/rentalSuggestionService.js
@@ -4,7 +4,11 @@ module.exports = {
     getSuggestedOffers: function (user) {
         generateSuggestedOffers(user);
 
-        return suggestedOffers;
+        if(Object.keys(user).length > 0) {
+            return suggestedOffers[user[0].token];
+        } else {
+            return suggestedOffers['other'];
+        }
     }
 };
 
@@ -15,15 +19,53 @@ function generateSuggestedOffers(user) {
     suggestedOffers = [];
 
     if (Object.keys(user).length > 0) {
-        suggestedOffers.push(filterHistoryByUserChoice(rentalHistory, user.token));
-        suggestedOffers.push(filterHistoryByOther(user));
+        var rentals = filterHistoryByUserChoice(rentalHistory, user[0].token)
+        rentals = groupByCar(rentals);
+        var rentalsOtherUsers = getPopularCarsForNonLoggedUser(rentalHistory);
+        suggestedOffers[user[0].token] = concatenateMaps(rentals, rentalsOtherUsers);
     } else {
-        suggestedOffers = getPopularCarsForNonLoggedUser(rentalHistory);
+        suggestedOffers['other'] = getPopularCarsForNonLoggedUser(rentalHistory);
     }
 
     return suggestedOffers;
 }
 
+function concatenateMaps(map1, map2) {
+    for(i = 0; i < map1.length; i++) {
+        for(j = 0; j < map2.length; j++) {
+            var car1 = map1[i];
+            var car2 = map2[j];
+            if(car1.id === car2.id) {
+                car1.count += car2.count;
+                car2.touched = true;
+            }
+        }
+    }
+    for(i = 0; i < map2.length; i++) {
+        car = map2[i];
+        if(car.touched === undefined) {
+            map1.push(car);
+        }
+    }
+    return sortByOrderHistory(map1);
+}
+
+function groupByCar(rentals) {
+    var rentalsMap = [];
+    rentals.map(function(rent) {
+        if (rentalsMap[rent.car.id] === undefined) {
+            var car = rent.car;
+            car.count = 1;
+            rentalsMap[rent.car.id] = car;
+        } else {
+            car = rentalsMap[rent.car.id];
+            car.count += 1;
+            rentalsMap[rent.car.id] = car;
+        }
+    });
+    rentalsMap = getRidOfKeys(rentalsMap);
+    return rentalsMap;
+}
 
 function filterHistoryByUserChoice(rentalHistory, userToken) {
     return rentalHistory.filter(function (item) {
@@ -31,14 +73,10 @@ function filterHistoryByUserChoice(rentalHistory, userToken) {
     });
 }
 
-function filterHistoryByOther(user){
-    //TODO:implement filtering order by rental count and !== current signed in user
-    return [];
-}
-
 function getPopularCarsForNonLoggedUser(rentalHistory) {
     var hist = [];
-    rentalHistory.map( function (rent) {
+    var copyHistory = JSON.parse(JSON.stringify(rentalHistory));
+    copyHistory.map( function (rent) {
         if (hist[rent.car.id] !== undefined) {
             car = hist[rent.car.id];
             car.count++;
@@ -74,7 +112,7 @@ function sortByOrderHistory(hist) {
         }
     } while(swapped);
 
-    return histOrdered;
+    return histOrdered.reverse();
 }
 
 function getRidOfKeys(hist) {
@@ -86,4 +124,3 @@ function getRidOfKeys(hist) {
     }
     return histWithoutKeys;
 }
-
